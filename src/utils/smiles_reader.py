3a1ee550e24826e94e2f02c8998e0f5c41569aee
Yang Liu <thomasliuy@gmail.com>
2019-09-30 15:34:11 -0400

    put read smiles from sdf code in function

--- src/utils/smiles_reader.py
+++ src/utils/smiles_reader.py
@@ -5,6 +5,21 @@ from random import sample, shuffle
 from paths import Path
 
 
+def get_smiles_from_sdf(path):
+    smiles = list()
+    with gzip.open(path, "rb") as sdf:
+        line = sdf.readline()
+        while line:
+            if b"ISO_SMILES" in line:
+                line = sdf.readline()
+                if len(line) > 100:
+                    line = sdf.readline()
+                    continue
+                smiles.append(line)
+            line = sdf.readline()
+    return smiles
+
+
 def smiles_sampler(n_samples):
     """ Sample n_samples isomeric SMILES string from pubChem dataset
     """
@@ -16,19 +31,12 @@ def smiles_sampler(n_samples):
     for gzf in files:
         if not gzf.name.startswith("Compound"):
             continue
-        with gzip.open(gzf.path, "rb") as sdf:
-            line = sdf.readline()
-            while line:
-                if b"ISO_SMILES" in line:
-                    line = sdf.readline()
-                    if len(line) > 100:
-                        line = sdf.readline()
-                        continue
-                    smiles.append(line)
-                    counter += 1
-                if counter == n_samples:
-                    break
-                line = sdf.readline()
+        smiles_in_gzf = get_smiles_from_sdf(gzf.path)
+        for ss in smiles_in_gzf:
+            smiles.append(ss)
+            counter += 1
+            if counter == n_samples:
+                break
         if counter == n_samples:
             break
 
@@ -44,6 +52,6 @@ def smiles_sampler(n_samples):
 
 
 if __name__ == "__main__":
-    gen = smiles_sampler(10)
+    gen = smiles_sampler(100)
     for i in range(20):
         print(next(gen))
